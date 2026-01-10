84f4e0f965c9d5b68040cf56303b751918759f3e
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-01-10 13:15:15 +0700

    add error template

--- docs/_next/static/chunks/37ffdf0d5d045623.js
+++ docs/_next/static/chunks/37ffdf0d5d045623.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/9b31403f01511912.js",
   "static/chunks/710f59839da85797.js",
-  "static/chunks/c7528e810a797988.css",
-  "static/chunks/turbopack-34c9521b7e44eb16.js"
+  "static/chunks/558477dbadff1b43.css",
+  "static/chunks/turbopack-57eddd5be5b516b8.js"
 ])
